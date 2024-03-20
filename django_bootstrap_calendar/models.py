736c95e36061af6e82b699feaf0daa6ca87723a7
Muthu Annamalai <photonique@hey.com>
2024-03-20 12:53:13 -0700

    - update code to work with Django LTS as of today - update code to work with various locale when LOCALE_CODE is set in project settings - update unicode (Python2) constructs to Python3 - update black code formatter

--- django_bootstrap_calendar/models.py
+++ django_bootstrap_calendar/models.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
-__author__ = 'sandlbn'
+__author__ = "sandlbn"
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from utils import datetime_to_timestamp
+from django.utils.translation import gettext_lazy as _
+from .utils import datetime_to_timestamp
 
 
 class CalendarEvent(models.Model):
     """
     Calendar Events
     """
+
     CSS_CLASS_CHOICES = (
-        ('', _('Normal')),
-        ('event-warning', _('Warning')),
-        ('event-info', _('Info')),
-        ('event-success', _('Success')),
-        ('event-inverse', _('Inverse')),
-        ('event-special', _('Special')),
-        ('event-important', _('Important')),
+        ("", _("Normal")),
+        ("event-warning", _("Warning")),
+        ("event-info", _("Info")),
+        ("event-success", _("Success")),
+        ("event-inverse", _("Inverse")),
+        ("event-special", _("Special")),
+        ("event-important", _("Important")),
     )
-    title = models.CharField(max_length=255, verbose_name=_('Title'))
-    url = models.URLField(verbose_name=_('URL'), null=True, blank=True)
-    css_class = models.CharField(blank=True, max_length=20, verbose_name=_('CSS Class'),
-                                 choices=CSS_CLASS_CHOICES)
-    start = models.DateTimeField(verbose_name=_('Start Date'))
-    end = models.DateTimeField(verbose_name=_('End Date'), null=True,
-                               blank=True)
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    url = models.URLField(verbose_name=_("URL"), null=True, blank=True)
+    css_class = models.CharField(
+        blank=True,
+        max_length=20,
+        verbose_name=_("CSS Class"),
+        choices=CSS_CLASS_CHOICES,
+    )
+    start = models.DateTimeField(verbose_name=_("Start Date"))
+    end = models.DateTimeField(verbose_name=_("End Date"), null=True, blank=True)
 
     @property
     def start_timestamp(self):
@@ -41,5 +45,5 @@ class CalendarEvent(models.Model):
         """
         return datetime_to_timestamp(self.end)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
--- django_bootstrap_calendar/urls.py
+++ django_bootstrap_calendar/urls.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
-__author__ = 'sandlbn'
+__author__ = "sandlbn"
 
-from django.conf.urls import url
-from views import CalendarJsonListView, CalendarView
+from django.urls import path
+from .views import CalendarJsonListView, CalendarView
 
 urlpatterns = [
-    url(
-        r'^json/$',
-        CalendarJsonListView.as_view(),
-        name='calendar_json'
-    ),
-    url(
-        r'^$',
-        CalendarView.as_view(),
-        name='calendar'
-    ),
+    path("json/", CalendarJsonListView.as_view(), name="calendar_json"),
+    path("", CalendarView.as_view(), name="calendar"),
 ]
